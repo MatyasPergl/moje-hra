84d0690d0024efda547a48911ad880e5dc2730fc
MatyasPergl <matyas.pergl@skolakrizik.cz>
2025-04-27 22:42:16 +0200

    Update RPGhra.cpp

--- moje-hra/RPGhra.cpp
+++ moje-hra/RPGhra.cpp
@@ -4,10 +4,19 @@ using namespace std;
 int atk;
 int armor;
 int HP;
+int MAXHP;
 int XP = 1;
 int MP;
 
-int vypis(){
+void pomoc(){
+	cout << "pomoc = vypise vsechny prikazy" << "\n";
+	cout << "vypis = vypise tvoje statistiky" << "\n";
+	cout << "boj = vypise tvoje utoky" << "\n";
+	cout << "pruzkum = posunes se dal v pribehu" << "\n";
+	cout << "batoh = vypise predmety co mas u sebe" << "\n";
+}
+
+void vypis(){
     cout << "Sila = " << atk << "\n";
     cout << "Zivoty = " << HP << "\n";
     cout << "Mana = " << MP << "\n";
@@ -15,6 +24,28 @@ int vypis(){
     cout << "Level = " << XP << "\n";
 }
 
+void boj(){
+	switch (claska){
+		case 1:
+            cout << "Si Mage a tvoje utoky jsou:\n";
+			cout << "Fireball = velke poskozeni na vsechny nepratele \n";
+            cout << "Stit = vyblokuje plne jeden utok nepritele \n";
+			break;
+		case 2:
+            cout << "Si Rogue a tvoje utoky jsou:\n";
+			cout << "Backstab = velke poskozeni na jednoho nepritele \n";
+			break;
+		case 3:
+		    cout << "Si Fighter a nemas specialni utoky :c\n";
+			break;
+		case 4:
+            cout << "Si Cleric a tvoje utoky jsou:\n";
+			cout << "Heal = vylecis si trochu zivotu \n";
+			break;
+	}
+}
+
+// fakt nevim jak udelam inventarovej system :sob: ale verim si sem sigma
 // HB Glacithorn
 int main(){
     int claska;
@@ -28,6 +59,7 @@ int main(){
             cout << "Stal se z tebe Mage!\n";
             atk = 3;
             armor = 1;
+			MAXHP = 10;
             HP = 10;
             MP = 20;
             break;
@@ -35,6 +67,7 @@ int main(){
             cout << "Stal se z tebe Rogue!\n";
             atk = 5;
             armor = 2;
+			MAXHP = 15;
             HP = 15;
             MP = 0;
             break;
@@ -42,6 +75,7 @@ int main(){
             cout << "Stal se z tebe Fighter!\n";
             atk = 3;
             armor = 3;
+			MAXHP = 20
             HP = 20;
             MP = 0;
             break;
@@ -49,6 +83,7 @@ int main(){
             cout << "Stal se z tebe Cleric!\n";
             atk = 1;
             armor = 5;
+			MAXHP = 10;
             HP = 10;
             MP = 20;
             break;
@@ -57,5 +92,5 @@ int main(){
             claska = 0;
     }
     }while(claska == 0);
-    cout << vypis();
+	pomoc();
 }
